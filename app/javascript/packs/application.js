96483df01ea224f4ef60694e5fe2bd06aa4d8e82
Tendai Nyandoro <tendai1@hotmail.com>
2021-01-26 09:39:30 +0200

    Refactor the jquery file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,25 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import 'jquery'
-import 'jquery-ujs';
+This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
 
-import 'bootstrap'
-import '../../assets/stylesheets/custom.scss'
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+import "bootstrap";
+import "./src/application.scss";
+global.$ = jQuery;
