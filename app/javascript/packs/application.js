e4b7addd7bcd261df74c16161d77438d64d17d47
Tendai Nyandoro <tendai1@hotmail.com>
2021-02-04 14:13:44 +0200

    Fix the home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,5 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
+import "@fortawesome/fontawesome-free/js/all";
 console.log('Hello World from Webpacker')
